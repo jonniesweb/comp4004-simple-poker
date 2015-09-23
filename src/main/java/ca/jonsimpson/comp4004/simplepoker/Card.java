d7cd10e89e97db3bef1e0d503a79059e4b6eb95c
Jon Simpson <jon@jonsimpson.ca>
2015-09-23 15:04:40 -0400

    Implement creating a Card from a String in RankSuit format

--- src/main/java/ca/jonsimpson/comp4004/simplepoker/Card.java
+++ src/main/java/ca/jonsimpson/comp4004/simplepoker/Card.java
@@ -39,8 +39,71 @@ public class Card {
 		
 	}
 	
-	public Card(String string) {
-		throw new UnsupportedOperationException();
+	/**
+	 * Create a card from a String of the RankSuit format. Eg. <code>4D</code>
+	 * is the Four of Diamonds.
+	 * 
+	 * Valid Ranks are <code>2 3 4 5 6 7 8 9 10 J Q K A</code>.
+	 * Valid Suits are <code>C H D S</code>.
+	 * 
+	 * @param rankSuit
+	 */
+	public Card(String rankSuit) {
+		// check input if null and length if too short or too long
+		if (rankSuit == null) {
+			throw new NullPointerException("rankSuit cannot be null");
+		} else if (rankSuit.length() < 2 || rankSuit.length() > 3) {
+			throw new InvalidCardException("Invalid RankSuit format");
+		}
+		
+		// grab the suit and rank out of the rankSuit String
+		char suit = rankSuit.charAt(rankSuit.length() - 1);
+		String rank = rankSuit.substring(0, rankSuit.length() - 1);
+		
+		// determine the Suit
+		if (suit == 'S') {
+			this.suit = Suit.SPADE;
+		} else if (suit == 'C') {
+			this.suit = Suit.CLUB;
+		} else if (suit == 'H') {
+			this.suit = Suit.HEART;
+		} else if (suit == 'D') {
+			this.suit = Suit.DIAMOND;
+		} else {
+			throw new InvalidCardException("Invalid suit specified: " + suit);
+		}
+		
+		// determine the Rank
+		if ("2".equals(rank)) {
+			this.rank = Rank.TWO;
+		} else if ("3".equals(rank)) {
+			this.rank = Rank.THREE;
+		} else if ("4".equals(rank)) {
+			this.rank = Rank.FOUR;
+		} else if ("5".equals(rank)) {
+			this.rank = Rank.FIVE;
+		} else if ("6".equals(rank)) {
+			this.rank = Rank.SIX;
+		} else if ("7".equals(rank)) {
+			this.rank = Rank.SEVEN;
+		} else if ("8".equals(rank)) {
+			this.rank = Rank.EIGHT;
+		} else if ("9".equals(rank)) {
+			this.rank = Rank.NINE;
+		} else if ("10".equals(rank)) {
+			this.rank = Rank.TEN;
+		} else if ("J".equals(rank)) {
+			this.rank = Rank.JACK;
+		} else if ("Q".equals(rank)) {
+			this.rank = Rank.QUEEN;
+		} else if ("K".equals(rank)) {
+			this.rank = Rank.KING;
+		} else if ("A".equals(rank)) {
+			this.rank = Rank.ACE;
+		} else {
+			throw new InvalidCardException("Invalid rank specified: " + rank);
+		}
+		
 	}
 
 	@Override
